acf4972723de71e94863e8d4663c0e660e7dc045
Nícolas Moura <nicolasmouramyssen123@gmail.com>
2025-10-08 20:56:18 -0300

    loja att 8

--- loja_app/views.py
+++ loja_app/views.py
@@ -3,15 +3,24 @@ from .models import Loja
 from .forms import LojaForm
 
 
+# ---------------------------
+# Página inicial
+# ---------------------------
 def home(request):
     return render(request, 'loja_app/home.html')
 
 
+# ---------------------------
+# Listar lojas
+# ---------------------------
 def lista_lojas(request):
     lojas = Loja.objects.all()
     return render(request, 'loja_app/loja_list.html', {'lojas': lojas})
 
 
+# ---------------------------
+# Cadastrar loja
+# ---------------------------
 def cadastrar_loja(request):
     if request.method == 'POST':
         form = LojaForm(request.POST)
@@ -20,11 +29,16 @@ def cadastrar_loja(request):
             return redirect('lista_lojas')
     else:
         form = LojaForm()
-    return render(request, 'loja_app/cadastrar_form.html', {'form': form})
+    return render(request, 'loja_app/loja_form.html', {'form': form})
 
+
+# ---------------------------
+# Editar loja
+# ---------------------------
 
 def editar_loja(request, id):
     loja = get_object_or_404(Loja, id=id)
+
     if request.method == 'POST':
         form = LojaForm(request.POST, instance=loja)
         if form.is_valid():
@@ -32,14 +46,17 @@ def editar_loja(request, id):
             return redirect('lista_lojas')
     else:
         form = LojaForm(instance=loja)
-    return render(request, 'loja_app/editar_form.html', {'form': form})
 
+    return render(request, 'loja_app/loja_form.html', {'form': form})
 
+# ---------------------------
+# Excluir loja
+# ---------------------------
 def excluir_loja(request, id):
     loja = get_object_or_404(Loja, id=id)
     if request.method == 'POST':
         loja.delete()
         return redirect('lista_lojas')
-    return render(request, 'loja_app/excluir_loja.html', {'loja': loja})
+    return render(request, 'loja_app/loja_confirm_delete.html', {'loja': loja})
 
 
